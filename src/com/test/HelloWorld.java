dbebeb288617c97845c6cc2ea5320b86db8afc58
lxrui0807 <13630221642@163.com>
2018-03-04 19:59:47 +0800

    Initial commit

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by Administrator on 2018/3/4.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
